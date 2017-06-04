f1bd528c80833d143126f874913289e095b3e8d5
Valentin Haenel <valentin@haenel.co>
2017-06-04 20:58:16 +0200

    implement fetchig

--- src/main/python/twentybn_dl/cli.py
+++ src/main/python/twentybn_dl/cli.py
@@ -3,10 +3,12 @@
 Usage:
     twentybn-dl get [<dataset>...]
     twentybn-dl extract [<dataset>...]
+    twentybn-dl fetch [<dataset>...]
 
 Subcommands:
     get : Download bigtgz file(s).
     extract: Extract the bigtgz file(s).
+    fetch: Download and extract the bigtgz file(s).
 
 """
 
@@ -23,7 +25,13 @@ def main():
 
     if arguments['get']:
         for d in dsets:
+            print("Will now get bigtgz for: '{}'".format(d.name))
             d.get_bigtgz()
     if arguments['extract']:
         for d in dsets:
+            print("Will now extract bigtgz for: '{}'".format(d.name))
+            d.extract_bigtgz()
+    if arguments['fetch']:
+        for d in dsets:
+            print("Will get and extract bigtgz for: '{}'".format(d.name))
             d.extract_bigtgz()
