e9db9aa0d5474bcb26d1de07abacb77d0e67b9e4
Valentin Haenel <valentin@haenel.co>
2017-06-04 16:07:50 +0200

    turn the script into a class

--- experimental/streaming_bigtgz_downloader.py
+++ experimental/streaming_bigtgz_downloader.py
@@ -2,7 +2,7 @@ import requests
 from urllib.request import urlopen
 from tqdm import tqdm
 
-BLOCKSIZE = 1024 * 8
+DEFAULT_BLOCKSIZE = 1024 * 8
 URL = "https://s3-eu-west-1.amazonaws.com/20bn-public-datasets/something-something/v0/"
 BIGTGZ = "20bn-something-something-v1.tgz"
 FILES = [
@@ -40,31 +40,52 @@ FILES = [
 ]
 
 
-def get_total_blocks_and_bytes():
-    total_blocks = 0
-    total_bytes = 0
-    for f in FILES:
-        remote_size = int(requests.head(URL+f).headers['Content-Length'])
-        total_bytes += remote_size
-        num_blocks, last_block_size = divmod(remote_size, BLOCKSIZE)
-        total_blocks += num_blocks
-        if last_block_size:
-            total_blocks += 1
-    return total_blocks, total_bytes
+class BigTGZStreamer(object):
+
+    def __init__(self,
+                 input_urls,
+                 input_urls_md5sums,
+                 output_file,
+                 output_md5sum,
+                 blocksize=DEFAULT_BLOCKSIZE,
+                 ):
+        self.input_urls = input_urls
+        self.input_urls_md5sums = input_urls_md5sums
+        self.output_file = output_file
+        self.output_md5sum = output_md5sum
+        self.blocksize = blocksize
+
+    def get_total_blocks_and_bytes(self):
+        total_blocks, total_bytes = 0, 0
+        for f in FILES:
+            remote_size = int(requests.head(URL+f).headers['Content-Length'])
+            total_bytes += remote_size
+            num_blocks, last_block_size = divmod(remote_size, self.blocksize)
+            total_blocks += num_blocks
+            if last_block_size:
+                total_blocks += 1
+        return total_blocks, total_bytes
+
+    def stream_to_file(self, url, file_pointer, pbar):
+        url_pointer = urlopen(url)
+        while True:
+            block = url_pointer.read(self.blocksize)
+            if not block:
+                break
+            file_pointer.write(block)
+            pbar.update(len(block))
+
+    def get(self):
+        with open(self.output_file, 'wb') as fp:
+            total_blocks, total_bytes = self.get_total_blocks_and_bytes()
+            pbar = tqdm(total=total_bytes,
+                        unit='bytes',
+                        ncols=80,
+                        unit_scale=True)
+            for u in self.input_urls:
+                self.stream_to_file(u, fp, pbar)
 
 
-def stream_to_file(url, file_pointer, pbar):
-    url_pointer = urlopen(url)
-    while True:
-        block = url_pointer.read(BLOCKSIZE)
-        if not block:
-            break
-        file_pointer.write(block)
-        pbar.update(len(block))
-
-
-with open(BIGTGZ, 'wb') as fp:
-    total_blocks, total_bytes = get_total_blocks_and_bytes()
-    pbar = tqdm(total=total_bytes, unit='bytes', ncols=80, unit_scale=True)
-    for f in FILES:
-        stream_to_file(URL+f, fp, pbar)
+if __name__ == '__main__':
+    btgzs = BigTGZStreamer([URL+f for f in FILES], None, BIGTGZ, None)
+    btgzs.get()
