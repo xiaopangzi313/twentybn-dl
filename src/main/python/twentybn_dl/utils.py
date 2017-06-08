2e208f0843b5121ed5b0720429781bc955d6beef
Valentin Haenel <valentin@haenel.co>
2017-06-08 13:47:02 +0200

    adding md5sum utils

---
+++ src/main/python/twentybn_dl/utils.py
@@ -0,0 +1,10 @@
+import hashlib
+
+
+def md5(filename):
+    hash_md5 = hashlib.md5()
+    with open(filename, "rb") as f:
+        for chunk in iter(lambda: f.read(4096), b""):
+            hash_md5.update(chunk)
+    return hash_md5.hexdigest()
+
