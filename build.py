3d9b419bee53c9c6619c54c3f20a6c513829d94c
Valentin Haenel <valentin@haenel.co>
2017-06-08 17:22:17 +0200

    better versioning

--- build.py
+++ build.py
@@ -1,4 +1,5 @@
 from pybuilder.core import use_plugin, init
+from pybuilder.vcs import count_travis
 
 use_plugin("python.core")
 use_plugin("python.unittest")
@@ -6,10 +7,12 @@ use_plugin("python.install_dependencies")
 use_plugin("python.flake8")
 use_plugin("python.coverage")
 use_plugin("python.distutils")
+use_plugin("filter_resources")
 
 
 name = "twentybn-dl"
 default_task = "publish"
+version = count_travis()
 
 
 @init
@@ -18,3 +21,5 @@ def set_properties(project):
     project.depends_on('tqdm')
     project.depends_on('docopt')
     project.depends_on('sh')
+    project.get_property('filter_resources_glob').extend(
+        ['**/twentybn_dl/__init__.py'])
