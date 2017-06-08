723cd630a1b1df51ce3f33be10f1ce24ef7bcb38
Valentin Haenel <valentin@haenel.co>
2017-06-08 15:10:44 +0200

    cleanup and refactor the command line interface

--- src/main/python/twentybn_dl/cli.py
+++ src/main/python/twentybn_dl/cli.py
@@ -1,32 +1,54 @@
 """twentybn-dl
 
 Usage:
-    twentybn-dl get-bigtgz [<dataset>...]
     twentybn-dl get-chunks [<dataset>...]
-    twentybn-dl md5-bigtgz [<dataset>...]
     twentybn-dl md5-chunks [<dataset>...]
-    twentybn-dl extract-bigtgz [<dataset>...]
     twentybn-dl extract-chunks [<dataset>...]
-    twentybn-dl concat-chunks [<dataset>...]
-    twentybn-dl fetch [<dataset>...]
     twentybn-dl remove-tmp [<dataset>...]
+    twentybn-dl obtain [<dataset>...]
+
+    twentybn-dl get-bigtgz [<dataset>...]
+    twentybn-dl md5-bigtgz [<dataset>...]
+    twentybn-dl extract-bigtgz [<dataset>...]
+    twentybn-dl concat-chunks [<dataset>...]
 
 Subcommands:
-    get-bigtgz : Download bigtgz file(s).
     get-chunks : Download bigtgz chunks.
     md5-chunks : Check the md5 sums for the chunks.
-    extract-bigtgz: Extract the bigtgz file(s).
     extract-chunks: Extract chunk file(s).
-    concat-chunks: Concatenate chunks into bigtgz
-    fetch: Download and extract the bigtgz file(s).
     remove-tmp: Remove all temporary files.
+    obtain: Download, extract and remove temporary files.
+
+    get-bigtgz : Download bigtgz file(s).
+    md5-bigtgz: Check md5 sum for the bigtg z file(s).
+    extract-bigtgz: Extract the bigtgz file(s).
+    concat-chunks: Concatenate chunks into bigtgz.
 
 """
-import sys
-
 from docopt import docopt
 
 from twentybn_dl.datasets import DATASETS_AVAILABLE
+
+
+def get_chunks(dsets):
+    for d in dsets:
+        print("Will now get chunks for: '{}'".format(d.name))
+        d.get_chunks()
+
+
+def md5_chunks(dsets):
+    for d in dsets:
+        print("Will check md5 sums for chunks for: '{}'".format(d.name))
+        ok = d.check_chunk_md5sum()
+        if not ok:
+            raise Exception("Some files failed their md5sum check, "
+                            "please see above and delete them manually.")
+
+
+def extract_chunks(dsets):
+    for d in dsets:
+        print("Will now extract chunks for: '{}'".format(d.name))
+        d.extract_chunks()
 
 
 def remove_tmp(dsets):
@@ -36,45 +58,29 @@ def remove_tmp(dsets):
 
 
 def main():
-    EXIT = 0
     arguments = docopt(__doc__)
     dsets = arguments['<dataset>'] or DATASETS_AVAILABLE.keys()
+    dsets = [DATASETS_AVAILABLE[d] for d in dsets]
+
+    if arguments['get-chunks']:
+        get_chunks(dsets)
+    if arguments['md5-chunks']:
+        md5_chunks()
+    if arguments['extract-chunks']:
+        extract_chunks(dsets)
+    if arguments['remove_tmp']:
+        remove_tmp()
+    if arguments['obtain']:
+        get_chunks(dsets)
+        md5_chunks()
+        extract_chunks(dsets)
+        remove_tmp()
 
     if arguments['get-bigtgz']:
         for d in dsets:
-            print("Will now get bigtgz for: '{}'".format(d))
-            s = DATASETS_AVAILABLE[d]
-            s.get_bigtgz()
-    if arguments['get-chunks']:
-        for d in dsets:
-            print("Will now get chunks for: '{}'".format(d))
-            s = DATASETS_AVAILABLE[d]
-            s.get_chunks()
+            print("Will now get bigtgz for: '{}'".format(d.name))
+            d.get_bigtgz()
     if arguments['extract-bigtgz']:
         for d in dsets:
-            print("Will now extract bigtgz for: '{}'".format(d))
-            s = DATASETS_AVAILABLE[d]
-            s.extract_bigtgz()
-    if arguments['extract-chunks']:
-        for d in dsets:
-            print("Will now extract chunks for: '{}'".format(d))
-            s = DATASETS_AVAILABLE[d]
-            s.extract_chunks()
-    if arguments['md5-chunks']:
-        for d in dsets:
-            print("Will check md5 sums for chunks for: '{}'".format(d))
-            s = DATASETS_AVAILABLE[d]
-            ok = s.check_chunk_md5sum()
-            if not ok:
-                print("Some files failed their md5sum check, please see above and delete them manually.")
-                EXIT = 1
-    if arguments['fetch']:
-        for d in dsets:
-            print("Will get and extract bigtgz for: '{}'".format(d))
-            s = DATASETS_AVAILABLE[d]
-            s.get_bigtgz()
-            s.extract_bigtgz()
-    if arguments['remove-tmp']:
-        remove_tmp([DATASETS_AVAILABLE[d] for d in dsets])
-
-    sys.exit(EXIT)
+            print("Will now extract bigtgz for: '{}'".format(d.name))
+            d.extract_bigtgz()
